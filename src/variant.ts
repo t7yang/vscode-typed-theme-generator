0de7fd8895655bbf415ec2d61a9136d046171261
t7yang <t7yang@gmail.com>
2021-11-16 12:35:24 +0800

    refactor: rename variable and type name for semantic

--- src/variant.ts
+++ src/variant.ts
@@ -2,19 +2,19 @@ import { TokenColor } from './token-colors';
 import { UiColor } from './ui-colors';
 
 type ObjKey = string | number;
-export type VariantColor<Variant extends ObjKey, Var extends ObjKey> = Record<Variant, Record<Var, string>>;
-export type VariantVar<Var extends ObjKey> = Record<Var, Var>;
+export type VariantColor<Label extends ObjKey, Var extends ObjKey> = Record<Label, Record<Var, string>>;
+export type VariantVariable<Var extends ObjKey> = Record<Var, Var>;
 
 /**
  * create variant theme color based variables
  * @param variants variant color variables
  * @returns variant color variable you input and variable tokens
  */
-export const createVariant = <Variant extends ObjKey, Var extends ObjKey>(
-  variants: VariantColor<Variant, Var>,
-): [VariantColor<Variant, Var>, VariantVar<Var>] => {
-  const vVar = Object.values(variants)[0] as VariantVar<Var> | undefined;
-  return [variants, (vVar ? Object.fromEntries(Object.keys(vVar).map(key => [key, key])) : {}) as VariantVar<Var>];
+export const createVariant = <Label extends ObjKey, Var extends ObjKey>(
+  variants: VariantColor<Label, Var>,
+): [VariantColor<Label, Var>, VariantVariable<Var>] => {
+  const VAR = Object.values(variants)[0] as VariantVariable<Var> | undefined;
+  return [variants, (VAR ? Object.fromEntries(Object.keys(VAR).map(key => [key, key])) : {}) as VariantVariable<Var>];
 };
 
 /**
@@ -23,21 +23,21 @@ export const createVariant = <Variant extends ObjKey, Var extends ObjKey>(
  * @param variants variant color variable return by createVariant function
  * @returns variant color based UI color tokens
  */
-export const createVariantUiColor = <Variant extends ObjKey, Var extends ObjKey>(
+export const createVariantUiColor = <Label extends ObjKey, Var extends ObjKey>(
   uiColor: UiColor,
-  variants: VariantColor<Variant, Var>,
-): Record<Variant, UiColor> => {
+  variants: VariantColor<Label, Var>,
+): Record<Label, UiColor> => {
   const variantUiColor = Object.fromEntries(Object.keys(variants).map(key => [key, { ...uiColor }])) as Record<
-    Variant,
+    Label,
     UiColor
   >;
 
   Object.entries(variants).forEach(variantEntry => {
-    const [variant, vVar] = variantEntry as [Variant, Record<Var, string>];
-    const themeUiColor = variantUiColor[variant];
+    const [label, VAR] = variantEntry as [Label, typeof variants[Label]];
+    const themeUiColor = variantUiColor[label];
     Object.entries(themeUiColor).forEach(themeEntry => {
       const [uiToken, color] = themeEntry as [keyof UiColor, string];
-      vVar[color as Var] && (themeUiColor[uiToken] = vVar[color as Var]);
+      VAR[color as Var] && (themeUiColor[uiToken] = VAR[color as Var]);
     });
   });
 
@@ -50,20 +50,20 @@ export const createVariantUiColor = <Variant extends ObjKey, Var extends ObjKey>
  * @param variants varaint color variable return by createVariant function
  * @returns variant color based syntax color tokens
  */
-export const createVariantTokenColors = <Variant extends ObjKey, Var extends ObjKey>(
+export const createVariantTokenColors = <Label extends ObjKey, Var extends ObjKey>(
   tokenColors: TokenColor[],
-  variants: VariantColor<Variant, Var>,
-): Record<Variant, TokenColor[]> => {
+  variants: VariantColor<Label, Var>,
+): Record<Label, TokenColor[]> => {
   const variantTokenColors = Object.fromEntries(
     Object.keys(variants).map(key => [key, tokenColors.map(settings => ({ ...settings }))]),
-  ) as Record<Variant, TokenColor[]>;
+  ) as Record<Label, TokenColor[]>;
 
   Object.entries(variants).forEach(variantEntry => {
-    const [variant, vVar] = variantEntry as [Variant, Record<Var, string>];
-    variantTokenColors[variant].forEach(tokenColor => {
+    const [label, VAR] = variantEntry as [Label, typeof variants[Label]];
+    variantTokenColors[label].forEach(tokenColor => {
       tokenColor.settings.foreground &&
-        vVar[tokenColor.settings.foreground as Var] &&
-        (tokenColor.settings = { ...tokenColor.settings, foreground: vVar[tokenColor.settings.foreground as Var] });
+        VAR[tokenColor.settings.foreground as Var] &&
+        (tokenColor.settings = { ...tokenColor.settings, foreground: VAR[tokenColor.settings.foreground as Var] });
     });
   });
 
