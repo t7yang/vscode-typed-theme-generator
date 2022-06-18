386a51f870df4303c47152611b61083acc4fd407
t7yang <t7yang@gmail.com>
2022-06-19 00:54:43 +0800

    fix: export `opacity`

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './generator';
 export * from './semantic-token-colors';
 export * from './theme';
 export * from './token-colors';
-export { transparent } from './transparent';
+export { opacity, transparent } from './transparent';
 export * from './ui-colors';
 export * from './variant';
